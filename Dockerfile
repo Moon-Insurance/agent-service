543f4b22cf776c382ac5de09206627c1462c806d
hasansin <hasansi1814@gmail.com>
2025-03-28 16:03:39 +0530

    added code changes for agent service

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 EXPOSE 5001
-# Entry point script to run migrations before starting the app
-CMD ["sh", "-c", "flask db upgrade && python app.py"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 #production, use environment variable for DB URI.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','sqlite:///:memory:')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # Initialize Migrations
@@ -29,7 +29,7 @@ def create_tables():
     db.create_all()
 
 # CREATE NEW AGENT
-@app.route('/agents', methods=['POST'])
+@app.route('/', methods=['POST'])
 def create_agent():
     data = request.json
     if not data or 'agent_id' not in data:
@@ -40,7 +40,7 @@ def create_agent():
     return jsonify(agent.as_dict()), 201
 
 # READ
-@app.route('/agents/<agent_id>', methods=['GET'])
+@app.route('/<agent_id>', methods=['GET'])
 def get_agent(agent_id):
     agent = Agent.query.filter_by(agent_id=agent_id).first()
     if not agent:
@@ -48,7 +48,7 @@ def get_agent(agent_id):
     return jsonify(agent.as_dict())
 
 # UPDATE
-@app.route('/agents/<agent_id>', methods=['PUT'])
+@app.route('/<agent_id>', methods=['PUT'])
 def update_agent(agent_id):
     data = request.json
     agent = Agent.query.filter_by(agent_id=agent_id).first()
@@ -60,7 +60,7 @@ def update_agent(agent_id):
     return jsonify(agent.as_dict())
 
 # DELETE AGENT
-@app.route('/agents/<agent_id>', methods=['DELETE'])
+@app.route('/<agent_id>', methods=['DELETE'])
 def delete_agent(agent_id):
     agent = Agent.query.filter_by(agent_id=agent_id).first()
     if not agent:
